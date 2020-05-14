bb22d81cd7e0dbc6fe4f512626e39b9a886cca2d
jnk52 <ajienkyanikam52@gmail.com>
2020-05-14 14:11:44 -0700

    uc3

--- Arithmetic comptatn 10-11/ArithmeticComputation.sh
+++ Arithmetic comptatn 10-11/ArithmeticComputation.sh
@@ -3,5 +3,7 @@ read -p "Enter the value a: " a
 read -p "Enter the value b: " b
 read -p "Enter the value c: " c
 
-Compute1=$((a+b*c))
+Compute1=$(($a+$b*$c))
 echo compute1
+compute2=$(($a * $b + c))
+echo compute2
